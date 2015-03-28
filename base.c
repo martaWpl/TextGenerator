85a98d63bf8e42d5b194e2c6909fa134b453a4e8
wisniem7 <marta.wisniewska19.10@gmail.com>
2015-03-28 23:30:50 +0100

    Pliki modułu base

---
+++ base.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "ngram.h"
+#include "base.h"
+
+void print_base( void *wsk_in, FILE *out);//tworzy bazę
+                                        //wsk_in wskaznik na kolejne pliki
+
